22603658ef49ad940b3b6ce40e020f6c3aaaf7c2
Samuel Friedman <sam@broadinstitute.org>
2018-05-02 14:53:05 -0400

    fix3

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariants.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariants.java
@@ -265,9 +265,11 @@ public class CNNScoreVariants extends VariantWalker {
             pythonExecutor.sendSynchronousCommand(String.format("tempFile = open('%s', 'w+')" + NL, scoreFile.getAbsolutePath()));
             pythonExecutor.sendSynchronousCommand("import vqsr_cnn" + NL);
             pythonExecutor.sendSynchronousCommand("from keras import backend" + NL);
-            pythonExecutor.sendSynchronousCommand(String.format("backend.set_session(backend.tf.Session(config=backend.tf.ConfigProto(intra_op_parallelism_threads=%d, inter_op_parallelism_threads=%d)))" + NL, tfIntraThreads, tfInterThreads));
+            //pythonExecutor.sendSynchronousCommand(String.format("backend.set_session(backend.tf.Session(config=backend.tf.ConfigProto(intra_op_parallelism_threads=%d, inter_op_parallelism_threads=%d)))" + NL, tfIntraThreads, tfInterThreads));
             pythonExecutor.sendSynchronousCommand("cfg = backend.tf.ConfigProto()" + NL);
             pythonExecutor.sendSynchronousCommand("cfg.gpu_options.allow_growth = True" + NL);
+            pythonExecutor.sendSynchronousCommand(String.format("cfg.intra_op_parallelism_threads=%d" + NL, tfIntraThreads));
+            pythonExecutor.sendSynchronousCommand(String.format("cfg.inter_op_parallelism_threads=%d" + NL, tfInterThreads));
             pythonExecutor.sendSynchronousCommand("backend.set_session(backend.tf.Session(config=cfg))" + NL);
 
             String getArgsAndModel;
@@ -421,8 +423,8 @@ public class CNNScoreVariants extends VariantWalker {
                 outputTensorsDir) + NL;
         pythonExecutor.sendAsynchronousCommand(pythonCommand);
         pythonExecutor.sendAsynchronousCommand("backend.clear_session()" + NL);
-        pythonExecutor.sendAsynchronousCommand("backend.get_session().close()" + NL);
-        pythonExecutor.sendAsynchronousCommand("backend.set_session(backend.tf.Session(config=cfg))" + NL);
+        //pythonExecutor.sendAsynchronousCommand("backend.get_session().close()" + NL);
+        //pythonExecutor.sendAsynchronousCommand("backend.set_session(backend.tf.Session(config=cfg))" + NL);
         asyncWriter.startAsynchronousBatchWrite(batchList);
 
     }

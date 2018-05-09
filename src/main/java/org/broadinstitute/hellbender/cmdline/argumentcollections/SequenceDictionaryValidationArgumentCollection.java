xusailor/gatk

prevent sequence dictionary validation when aligning reads
previously, tools that align reads required you to manually disable sequence dictionary validation
if you didn't, they would fail because the unaligned bam didn't have the required sequence dictionary

extracting out a SequenceDictionaryValidationArgumentCollection and providing a method for GATKSparkTools to configure it
ReadsPipeline couldn't easily make use of this, so instead it overrides the method that does validation

BwaSpark / BwaAndMarkDuplicatesPipelineSpark now do not require or allow dictionary validation
fixes #4131

src/main/java/org/broadinstitute/hellbender/engine/spark/SequenceDictionaryValidationArgumentCollection.java
@@ -1,0 +1,42 @@
+package org.broadinstitute.hellbender.engine.spark;
+
+import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+
+/**
+ * interface for argument collections that control
+ */
+public interface SequenceDictionaryValidationArgumentCollection {
+
+    /**
+     * Should sequence dictionary validation be performed
+     * @return true if the tool should perform sequence dictionary validation
+     */
+    boolean performSequenceDictionaryValidation();
+
+
+    /**
+     * most tools will want to use this, it defaults to performing sequence dictionary validation but provides the option
+     * to disable it
+     */
+    class StandardValidationCollection implements SequenceDictionaryValidationArgumentCollection {
+        @Argument(fullName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, shortName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, doc = "If specified, do not check the sequence dictionaries from our inputs for compatibility. Use at your own risk!", optional = true)
+        private boolean disableSequenceDictionaryValidation = false;
+
+        @Override
+        public boolean performSequenceDictionaryValidation() {
+            return !disableSequenceDictionaryValidation;
+        }
+    }
+
+    /**
+     * doesn't provide a configuration argument, and always returns false, useful for tools that do not want to perform
+     * sequence dictionary validation, like aligners
+     */
+    class NoValidationCollection implements SequenceDictionaryValidationArgumentCollection {
+        @Override
+        public boolean performSequenceDictionaryValidation() {
+            return false;
+        }
+    }
+}

src/main/java/org/broadinstitute/hellbender/engine/GATKTool.java
@@ -21,7 +21,7 @@ import org.broadinstitute.hellbender.engine.filters.CountingReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary;
 import org.broadinstitute.hellbender.engine.filters.WellformedReadFilter;
-import org.broadinstitute.hellbender.exceptions.GATKException;
+import org.broadinstitute.hellbender.engine.spark.SequenceDictionaryValidationArgumentCollection;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.transformers.ReadTransformer;
 import org.broadinstitute.hellbender.utils.SequenceDictionaryUtils;
@@ -67,8 +67,8 @@ public abstract class GATKTool extends CommandLineProgram {
     @Argument(fullName = SECONDS_BETWEEN_PROGRESS_UPDATES_NAME, shortName = SECONDS_BETWEEN_PROGRESS_UPDATES_NAME, doc = "Output traversal statistics every time this many seconds elapse", optional = true, common = true)
     private double secondsBetweenProgressUpdates = ProgressMeter.DEFAULT_SECONDS_BETWEEN_UPDATES;
 
-    @Argument(fullName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, shortName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, doc = "If specified, do not check the sequence dictionaries from our inputs for compatibility. Use at your own risk!", optional = true, common = true)
-    private boolean disableSequenceDictionaryValidation = false;
+    @ArgumentCollection
+    SequenceDictionaryValidationArgumentCollection seqValidationArguments = new SequenceDictionaryValidationArgumentCollection.StandardValidationCollection();
 
     @Argument(fullName=StandardArgumentDefinitions.CREATE_OUTPUT_BAM_INDEX_LONG_NAME,
             shortName=StandardArgumentDefinitions.CREATE_OUTPUT_BAM_INDEX_SHORT_NAME,
@@ -559,7 +559,7 @@ public abstract class GATKTool extends CommandLineProgram {
 
         initializeIntervals(); // Must be initialized after reference, reads and features, since intervals currently require a sequence dictionary from another data source
 
-        if ( ! disableSequenceDictionaryValidation ) {
+        if ( seqValidationArguments.performSequenceDictionaryValidation()) {
             validateSequenceDictionaries();
         }
 

src/main/java/org/broadinstitute/hellbender/tools/spark/bwa/BwaSpark.java
@@ -8,6 +8,7 @@ import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.spark.SequenceDictionaryValidationArgumentCollection;
 import picard.cmdline.programgroups.ReadDataManipulationProgramGroup;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary;
@@ -22,8 +23,8 @@ import java.util.Arrays;
 import java.util.List;
 
 @DocumentedFeature
-@CommandLineProgramProperties(summary = "Runs BWA",
-        oneLineSummary = "BWA on Spark",
+@CommandLineProgramProperties(summary = "align reads using BWA",
+        oneLineSummary = "align reads to a given reference using BWA on Spark",
         programGroup = ReadDataManipulationProgramGroup.class)
 @BetaFeature
 public final class BwaSpark extends GATKSparkTool {
@@ -52,6 +53,11 @@ public final class BwaSpark extends GATKSparkTool {
     public List<ReadFilter> getDefaultReadFilters() {
         // 1) unmapped or neither secondary nor supplementary and 2) has some sequence
         return Arrays.asList(ReadFilterLibrary.PRIMARY_LINE, ReadFilterLibrary.SEQ_IS_STORED);
+    }
+
+    @Override
+    public SequenceDictionaryValidationArgumentCollection getSequenceDictionaryValidationArgumentCollection(){
+        return new SequenceDictionaryValidationArgumentCollection.NoValidationCollection();
     }
 
     @Override

src/main/java/org/broadinstitute/hellbender/tools/spark/pipelines/BwaAndMarkDuplicatesPipelineSpark.java
@@ -8,6 +8,7 @@ import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.spark.SequenceDictionaryValidationArgumentCollection;
 import picard.cmdline.programgroups.ReadDataManipulationProgramGroup;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.spark.GATKSparkTool;
@@ -53,6 +54,11 @@ public final class BwaAndMarkDuplicatesPipelineSpark extends GATKSparkTool {
     @Argument(doc = "the output bam", shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
             fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME)
     protected String output;
+
+    @Override
+    protected SequenceDictionaryValidationArgumentCollection getSequenceDictionaryValidationArgumentCollection() {
+        return new SequenceDictionaryValidationArgumentCollection.NoValidationCollection();
+    }
 
     @Override
     protected void runTool(final JavaSparkContext ctx) {

src/test/java/org/broadinstitute/hellbender/engine/spark/SequenceDictionaryValidationArgumentCollectionTest.java
@@ -1,0 +1,36 @@
+package org.broadinstitute.hellbender.engine.spark;
+
+import org.broadinstitute.barclay.argparser.ArgumentCollection;
+import org.broadinstitute.barclay.argparser.CommandLineArgumentParser;
+import org.broadinstitute.barclay.argparser.CommandLineParser;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SequenceDictionaryValidationArgumentCollectionTest {
+
+    private static class StandardArgumentCollection {
+        @ArgumentCollection
+        public SequenceDictionaryValidationArgumentCollection standard = new SequenceDictionaryValidationArgumentCollection.StandardValidationCollection();
+    }
+
+    @Test
+    public void testStandardArgumentCollectionDefaultsToTrue(){
+        Assert.assertTrue(new SequenceDictionaryValidationArgumentCollection.StandardValidationCollection().performSequenceDictionaryValidation());
+    }
+
+    @Test
+    public void testStandardArgumentCollectionCanBeDisabled(){
+        final String[] disabled = {"--"+StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME};
+        StandardArgumentCollection std = new StandardArgumentCollection();
+        CommandLineParser clp = new CommandLineArgumentParser(std);
+        clp.parseArguments(System.out, disabled);
+        Assert.assertFalse(std.standard.performSequenceDictionaryValidation());
+    }
+
+    @Test
+    public void testNoValidationDefaultsToFalse(){
+        Assert.assertFalse(new SequenceDictionaryValidationArgumentCollection.NoValidationCollection().performSequenceDictionaryValidation());
+    }
+
+}

singerma/gatk-1

src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/SequenceDictionaryValidationArgumentCollection.java
@@ -1,0 +1,42 @@
+package org.broadinstitute.hellbender.cmdline.argumentcollections;
+
+import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+
+/**
+ * interface for argument collections that control
+ */
+public interface SequenceDictionaryValidationArgumentCollection {
+
+    /**
+     * Should sequence dictionary validation be performed
+     * @return true if the tool should perform sequence dictionary validation
+     */
+    boolean performSequenceDictionaryValidation();
+
+
+    /**
+     * most tools will want to use this, it defaults to performing sequence dictionary validation but provides the option
+     * to disable it
+     */
+    class StandardValidationCollection implements SequenceDictionaryValidationArgumentCollection {
+        @Argument(fullName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, shortName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, doc = "If specified, do not check the sequence dictionaries from our inputs for compatibility. Use at your own risk!", optional = true)
+        private boolean disableSequenceDictionaryValidation = false;
+
+        @Override
+        public boolean performSequenceDictionaryValidation() {
+            return !disableSequenceDictionaryValidation;
+        }
+    }
+
+    /**
+     * doesn't provide a configuration argument, and always returns false, useful for tools that do not want to perform
+     * sequence dictionary validation, like aligners
+     */
+    class NoValidationCollection implements SequenceDictionaryValidationArgumentCollection {
+        @Override
+        public boolean performSequenceDictionaryValidation() {
+            return false;
+        }
+    }
+}

src/main/java/org/broadinstitute/hellbender/engine/GATKTool.java
@@ -21,6 +21,7 @@ import org.broadinstitute.hellbender.engine.filters.CountingReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary;
 import org.broadinstitute.hellbender.engine.filters.WellformedReadFilter;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.SequenceDictionaryValidationArgumentCollection;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.transformers.ReadTransformer;
 import org.broadinstitute.hellbender.utils.SequenceDictionaryUtils;
@@ -66,8 +67,8 @@ public abstract class GATKTool extends CommandLineProgram {
     @Argument(fullName = SECONDS_BETWEEN_PROGRESS_UPDATES_NAME, shortName = SECONDS_BETWEEN_PROGRESS_UPDATES_NAME, doc = "Output traversal statistics every time this many seconds elapse", optional = true, common = true)
     private double secondsBetweenProgressUpdates = ProgressMeter.DEFAULT_SECONDS_BETWEEN_UPDATES;
 
-    @Argument(fullName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, shortName = StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME, doc = "If specified, do not check the sequence dictionaries from our inputs for compatibility. Use at your own risk!", optional = true, common = true)
-    private boolean disableSequenceDictionaryValidation = false;
+    @ArgumentCollection
+    SequenceDictionaryValidationArgumentCollection seqValidationArguments = new SequenceDictionaryValidationArgumentCollection.StandardValidationCollection();
 
     @Argument(fullName=StandardArgumentDefinitions.CREATE_OUTPUT_BAM_INDEX_LONG_NAME,
             shortName=StandardArgumentDefinitions.CREATE_OUTPUT_BAM_INDEX_SHORT_NAME,
@@ -558,7 +559,7 @@ public abstract class GATKTool extends CommandLineProgram {
 
         initializeIntervals(); // Must be initialized after reference, reads and features, since intervals currently require a sequence dictionary from another data source
 
-        if ( ! disableSequenceDictionaryValidation ) {
+        if ( seqValidationArguments.performSequenceDictionaryValidation()) {
             validateSequenceDictionaries();
         }
 

src/main/java/org/broadinstitute/hellbender/tools/spark/bwa/BwaSpark.java
@@ -8,6 +8,7 @@ import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.SequenceDictionaryValidationArgumentCollection;
 import picard.cmdline.programgroups.ReadDataManipulationProgramGroup;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary;
@@ -22,8 +23,8 @@ import java.util.Arrays;
 import java.util.List;
 
 @DocumentedFeature
-@CommandLineProgramProperties(summary = "Runs BWA",
-        oneLineSummary = "BWA on Spark",
+@CommandLineProgramProperties(summary = "align reads using BWA",
+        oneLineSummary = "align reads to a given reference using BWA on Spark",
         programGroup = ReadDataManipulationProgramGroup.class)
 @BetaFeature
 public final class BwaSpark extends GATKSparkTool {
@@ -52,6 +53,11 @@ public final class BwaSpark extends GATKSparkTool {
     public List<ReadFilter> getDefaultReadFilters() {
         // 1) unmapped or neither secondary nor supplementary and 2) has some sequence
         return Arrays.asList(ReadFilterLibrary.PRIMARY_LINE, ReadFilterLibrary.SEQ_IS_STORED);
+    }
+
+    @Override
+    public SequenceDictionaryValidationArgumentCollection getSequenceDictionaryValidationArgumentCollection(){
+        return new SequenceDictionaryValidationArgumentCollection.NoValidationCollection();
     }
 
     @Override

src/main/java/org/broadinstitute/hellbender/tools/spark/pipelines/BwaAndMarkDuplicatesPipelineSpark.java
@@ -8,6 +8,7 @@ import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.SequenceDictionaryValidationArgumentCollection;
 import org.broadinstitute.hellbender.cmdline.argumentcollections.MarkDuplicatesSparkArgumentCollection;
 import picard.cmdline.programgroups.ReadDataManipulationProgramGroup;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
@@ -55,6 +56,11 @@ public final class BwaAndMarkDuplicatesPipelineSpark extends GATKSparkTool {
     @ArgumentCollection
     protected MarkDuplicatesSparkArgumentCollection markDuplicatesSparkArgumentCollection = new MarkDuplicatesSparkArgumentCollection();
 
+
+    @Override
+    protected SequenceDictionaryValidationArgumentCollection getSequenceDictionaryValidationArgumentCollection() {
+        return new SequenceDictionaryValidationArgumentCollection.NoValidationCollection();
+    }
 
     @Override
     protected void runTool(final JavaSparkContext ctx) {

src/test/java/org/broadinstitute/hellbender/cmdline/argumentcollections/SequenceDictionaryValidationArgumentCollectionTest.java
@@ -1,0 +1,37 @@
+package org.broadinstitute.hellbender.cmdline.argumentcollections;
+
+import org.broadinstitute.barclay.argparser.ArgumentCollection;
+import org.broadinstitute.barclay.argparser.CommandLineArgumentParser;
+import org.broadinstitute.barclay.argparser.CommandLineParser;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.SequenceDictionaryValidationArgumentCollection;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SequenceDictionaryValidationArgumentCollectionTest {
+
+    private static class StandardArgumentCollection {
+        @ArgumentCollection
+        public SequenceDictionaryValidationArgumentCollection standard = new SequenceDictionaryValidationArgumentCollection.StandardValidationCollection();
+    }
+
+    @Test
+    public void testStandardArgumentCollectionDefaultsToTrue(){
+        Assert.assertTrue(new SequenceDictionaryValidationArgumentCollection.StandardValidationCollection().performSequenceDictionaryValidation());
+    }
+
+    @Test
+    public void testStandardArgumentCollectionCanBeDisabled(){
+        final String[] disabled = {"--"+StandardArgumentDefinitions.DISABLE_SEQUENCE_DICT_VALIDATION_NAME};
+        StandardArgumentCollection std = new StandardArgumentCollection();
+        CommandLineParser clp = new CommandLineArgumentParser(std);
+        clp.parseArguments(System.out, disabled);
+        Assert.assertFalse(std.standard.performSequenceDictionaryValidation());
+    }
+
+    @Test
+    public void testNoValidationDefaultsToFalse(){
+        Assert.assertFalse(new SequenceDictionaryValidationArgumentCollection.NoValidationCollection().performSequenceDictionaryValidation());
+    }
+
+}
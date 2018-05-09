1034ab856b6187202bae9ff7983325b52651bce5
Samuel Friedman <sam@broadinstitute.org>
2018-05-09 10:55:31 -0400

    mitochondria bugfix

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariantsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariantsIntegrationTest.java
@@ -88,15 +88,25 @@ public class CNNScoreVariantsIntegrationTest extends CommandLineProgramTest {
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
         argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, inputVCF)
                 .addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s")
-                .addArgument(StandardArgumentDefinitions.REFERENCE_LONG_NAME, b37_reference_20_21)
-                .addArgument("architecture", architecture1D)
-                .addArgument("inference-batch-size", "8")
-                .addArgument("transfer-batch-size", "16")
+                .addArgument(StandardArgumentDefinitions.REFERENCE_LONG_NAME, hg19MiniReference)
                 .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
 
         final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
                 Arrays.asList(largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf"));
         spec.executeTest("testInference", this);
+    }
+
+    @Test(groups = {"python"})
+    public void testOnMitochondria() throws IOException{
+        final String mitoVcf = largeFileTestDir + "VQSR/errorM.vcf";
+        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
+        argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, mitoVcf)
+                .addArgument(StandardArgumentDefinitions.REFERENCE_LONG_NAME, largeFileTestDir +"mtDNA.fa")
+                .addArgument("architecture", architecture1D)
+                .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
+
+        argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf");
+        runCommandLine(argsBuilder);
     }
 
     /**
